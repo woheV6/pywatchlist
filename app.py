bc2c8c3f5a2096eefe0d5dde5a7064802fe2ae6f
hehong <wohev5@163.com>
2019-03-22 09:32:05 +0800

    /

--- app.py
+++ app.py
@@ -1,10 +1,23 @@
-from flask import Flask,url_for
+from flask import Flask,url_for,render_template
 app = Flask(__name__)
+name = 'Grey Li'
+movies = [
+    {'title': 'My Neighbor Totoro', 'year': '1988'},
+    {'title': 'Dead Poets Society', 'year': '1989'},
+    {'title': 'A Perfect World', 'year': '1993'},
+    {'title': 'Leon', 'year': '1994'},
+    {'title': 'Mahjong', 'year': '1996'},
+    {'title': 'Swallowtail Butterfly', 'year': '1996'},
+    {'title': 'King of Comedy', 'year': '1999'},
+    {'title': 'Devils on the Doorstep', 'year': '1999'},
+    {'title': 'WALL-E', 'year': '2008'},
+    {'title': 'The Pork of Music', 'year': '2012'},
+]
 @app.route('/') # 装饰器
 @app.route('/index') # 装饰器
 @app.route('/home') # 装饰器
 def main():
-    return 'Welcome to my watchlist!!'
+    return render_template('index.html',name=name,movies=movies)
 @app.route('/test')
 def test():
     return  '<h1>哈哈</h1><img src="http://helloflask.com/totoro.gif">'
